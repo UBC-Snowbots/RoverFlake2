29fc291149983af84d4fb120ec45126e79273b74
Roozki <zawadzkirowan@gmail.com>
2024-03-16 14:19:48 -0700

    experimenting with i3 bash scripts

---
+++ src/rover_manager/screenScripts/startup_system.sh
@@ -0,0 +1,10 @@
+#To startup the whole system
+source cb_env_variables.sh
+clear
+echo CONTROL BASE SYSTEM START | figlet | lolcat
+sleep 0.5
+echo MIDDLE SCREEN: $cb_MIDDLE_SCREEN_ID, $cb_MIDDLE_SCREEN_RES
+echo LEFT SCREEN: $cb_LEFT_SCREEN_ID, $cb_LEFT_SCREEN_RES
+echo RIGHT SCREEN: $cb_RIGHT_SCREEN_ID, $cb_RIGHT_SCREEN_RES
+
+
---
+++ src/rover_manager/screenScripts/xrandr_setup_g15.sh
@@ -0,0 +1,1 @@
+xrandr --output HDMI-A-0 --mode 1600x900 --right-of eDP
