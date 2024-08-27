8fe27cbdc69d280dbbcb0cfcf55094660c3fabc0
Cameron-Basara <cameronbasara@gmail.com>
2024-08-27 14:30:41 -0700

    removed unneeded files

--- src/colour_classifier/colour_classifier/light_classification_node.py
+++
@@ -1,6 +0,0 @@
-"""
-Created by: Cameron Basara
-Date Created: 3/5/2024
-Purpose: To classify different light sources, and publish the most probable colour to a topic
-"""
-#!/usr/bin/env python3
--- src/sample_cppython/sample_cppython/module_to_import.py
+++
@@ -1,8 +0,0 @@
-"""
-Name: Cameron Basara
-Date: 3/11/2024
-Purpose: Sample python mod for a sample package to for copying (mixed cpp/python)
-"""
-#!/usr/bin/env python3
-
-# Import libraries ....
