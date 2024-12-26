2691214ad3fac6db1330e4c3de58b5e2674bc714
Roozki <zawadzkirowan@gmail.com>
2024-12-25 18:32:42 -0800

    Watchdog can now check how many nodes are running Next step is what nodes are running

--- src/rover_manager/include/watchdogNode.h
+++ src/rover_manager/include/watchdogNode.h
@@ -1,5 +1,27 @@
 #include "rclcpp/rclcpp.hpp"
 
 
+class WatchdogNode : public rclcpp::Node
+{
+public:
+    WatchdogNode() : Node("Watchdog"){
+               // Create a timer that fires every 500 milliseconds
+    healthCheckTimer = this->create_wall_timer(
+        std::chrono::milliseconds(1000),  // Timer interval
+        std::bind(&WatchdogNode::cycleNodeHealth, this) // Callback function
+    );
+    }
+
+    ~WatchdogNode(){
+        RCLCPP_WARN(this->get_logger(), "WARNING: WATCHDOG NODE OFFLINE!");
+    }
+
+    void cycleNodeHealth();
+private:
+
+rclcpp::TimerBase::SharedPtr healthCheckTimer; // Timer handle
+
+
+};
 
 
--- src/rover_manager/src/watchdogNode.cpp
+++ src/rover_manager/src/watchdogNode.cpp
@@ -1,10 +1,26 @@
 #include <watchdogNode.h>
 
 
+void WatchdogNode::cycleNodeHealth(){
+auto nodes_currently_running = this->get_node_graph_interface()->get_node_names_and_namespaces();
 
-int main(char argc, char *argv[]){
+int num_current_nodes = nodes_currently_running.size();
+
+RCLCPP_INFO(this->get_logger(), "Nodes Currently Running: %i", num_current_nodes);
+
+// nodes_currently_running.
+
+}
+
+
+int main(int argc, char *argv[]){
     rclcpp::init(argc, argv); 
-
+    auto node = std::make_shared<WatchdogNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
 
     return 0;
 }
+
+
+
