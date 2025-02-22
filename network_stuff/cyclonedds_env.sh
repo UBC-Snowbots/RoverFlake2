5c280e6561fc48e112b3d3bdacaed855bd364509
Roozki <zawadzkirowan@gmail.com>
2025-02-21 22:11:52 -0800

    cyclonedds config

---
+++ network_stuff/cyclonedds_env.sh
@@ -0,0 +1,8 @@
+export ROS_DOMAIN_ID=101
+export ONBOARDIP=10.147.20.11
+export ROS_LOCALHOST_ONLY=0
+export ROS_INTERFACE=ztwdjkvykg
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export CYCLONEDDS_URI=file://${HOME}/RoverFlake2/network_stuff/dds_profile_cyclone.xml
+# export CYCLONEDDS_URI=file:///home/rowan/.config/ros2_rowan/dds_profile_cyclone.xml
+export RDS_HUD_WORKSPACE=5
