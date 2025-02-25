aa6d64640aa73a6cad4d9933887532d65b7933e8
melontop <wflyerthariani@gmail.com>
2025-02-25 14:04:47 -0800

    Required for nav2 atm

--- src/rover_urdf/launch/slam_launch.py
+++ src/rover_urdf/launch/slam_launch.py
@@ -5,16 +5,21 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package="slam_toolbox",
-            executable="sync_slam_toolbox_node",
+            executable="async_slam_toolbox_node",
             name="slam_toolbox",
             output="screen",
             parameters=[{
-                "use_sim_time": False,
+                "use_sim_time": True,
                 "map_frame": "map",
                 "odom_frame": "odom",
                 "base_frame": "base_link",
                 "scan_topic": "/scan",
-                "mode": "mapping"
+                "mode": "mapping",
+                "transform_tolerance": 3,
+                "transform_cache_size": 1000,
+                "transform_publish_period": 0.0,
+                "min_laser_range": 1.0,
+                "max_laser_range": 10.0
             }]
         )
     ])
