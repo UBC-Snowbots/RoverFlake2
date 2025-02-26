c05ff78298a6de8f38c3b3e14c40377704129c85
melontop <wflyerthariani@gmail.com>
2025-02-25 20:04:16 -0800

    Stuff for video

--- src/rover_urdf/launch/slam_launch.py
+++ src/rover_urdf/launch/slam_launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
             name="slam_toolbox",
             output="screen",
             parameters=[{
-                "use_sim_time": True,
+                "use_sim_time": False,
                 "map_frame": "map",
                 "odom_frame": "odom",
                 "base_frame": "base_link",
@@ -35,12 +35,20 @@ def generate_launch_description():
 >>>>>>> e627b7e (Add odom and map transform lauch files, timestamp issue persists)
 =======
                 "mode": "mapping",
+<<<<<<< HEAD
                 "transform_tolerance": 3,
                 "transform_cache_size": 1000,
                 "transform_publish_period": 0.0,
                 "min_laser_range": 1.0,
                 "max_laser_range": 10.0
 >>>>>>> aa6d646 (Required for nav2 atm)
+=======
+                "transform_tolerance": 1,
+                "transform_cache_size": 10000,
+                "transform_publish_period": 0.05,
+                "min_laser_range": 0.0,
+                "max_laser_range": 25.0
+>>>>>>> 7fb6a96 (Stuff for video)
             }]
         )
     ])
