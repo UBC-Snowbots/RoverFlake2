18b073ed7c136a485171234e4b37bad2d4b15316
Roozki <zawadzkirowan@gmail.com>
2025-03-10 11:47:26 -0700

    Forks as a way for watchdog / dashboard launcher?

--- src/rover_hmi/include/dashboardHMI.h
+++ src/rover_hmi/include/dashboardHMI.h
@@ -1,6 +1,12 @@
 #include <HMICommon.h>
-#define NUM_MONITORED_NODES 5
+#define NUM_MONITORED_SYSTEMS 6
 // #include <helper_functions.h>
+
+//? Watchdog stuffs..?
+#include <unistd.h>     // For fork, execlp
+#include <sys/types.h>  // For pid_t
+#include <stdlib.h>     // For exit
+
 
 class DashboardHMINode : public rclcpp::Node, public Gtk::Window
 {
@@ -23,24 +29,49 @@ public:
         //* Glade builder setup
         builder->get_widget("dash_window", dash_window);
         builder->get_widget("dash_top_layout", dash_layout);
-            //* Node Status Grid
-            builder->get_widget("node_status_grid", node_stats.grid);
-                builder->get_widget("node_0_label", node_stats.nodes[0].name);
-                builder->get_widget("node_1_label", node_stats.nodes[1].name);
-                builder->get_widget("node_0_status_label", node_stats.nodes[0].status);
-                builder->get_widget("node_1_status_label", node_stats.nodes[1].status);
+            //* subsys Status Grid
+            builder->get_widget("subsys_status_grid", system_health.grid);
+                builder->get_widget("subsys_0_label", system_health.system[0].name);
+                builder->get_widget("subsys_1_label", system_health.system[1].name);
+                builder->get_widget("subsys_2_label", system_health.system[2].name);
+                builder->get_widget("subsys_3_label", system_health.system[3].name);
+                builder->get_widget("subsys_4_label", system_health.system[4].name);
+                builder->get_widget("subsys_5_label", system_health.system[5].name);
+                builder->get_widget("subsys_0_status_label", system_health.system[0].status);
+                builder->get_widget("subsys_1_status_label", system_health.system[1].status);
+                builder->get_widget("subsys_2_status_label", system_health.system[2].status);
+                builder->get_widget("subsys_3_status_label", system_health.system[3].status);
+                builder->get_widget("subsys_4_status_label", system_health.system[4].status);
+                builder->get_widget("subsys_5_status_label", system_health.system[5].status);
 
-                node_stats.nodes[0].name->set_label(monitored_nodes[0]);
-                node_stats.nodes[1].name->set_label(monitored_nodes[1]);
+            
 
-                for(int i = 0; i < 2; i++){
-                    node_stats.nodes[i].name->set_label(monitored_nodes[i]);
-                    node_stats.nodes[i].status->set_label("UNKNOWN");
+                for(int i = 0; i < NUM_MONITORED_SYSTEMS; i++){
+                    system_health.system[i].name->set_label(monitored_systems[i]);
+                    system_health.system[i].status->set_label("UNKNOWN");
                 }
         
 
 
         RCLCPP_INFO(this->get_logger(), "BUILDER SUCCESS");
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            // Child process: replace this process with "ros2 run my_package my_node"
+            execlp("ros2", "ros2", "run", "joy", "joy_node", (char *)NULL);
+    
+            // If execlp() returns, there was an error
+            perror("execlp failed");
+            exit(EXIT_FAILURE);
+    
+        } else if (pid < 0) {
+            // fork() error in parent
+            perror("fork failed");
+        } else {
+            // Parent process: fork succeeded
+            // 'pid' is the child's PID. We could store it if we want to terminate later.
+            printf("Launched process with PID: %d\n", pid);
+        }
     }
 
     ~DashboardHMINode(){
@@ -59,7 +90,7 @@ public:
 private:
     std::string main_css_file_path;
     std::string package_share_dir;
-    std::vector<std::string> monitored_nodes = {"cbs_daemon", "drive_control"};
+    std::vector<std::string> monitored_systems = {"cbs_daemon", "drive_control", "arm_hardware", "arm_control", "science", "perceptions"};
     
 
 
@@ -69,21 +100,21 @@ private:
     Gtk::Layout* dash_layout;
 
 
-  struct NodeStatusElement
+  struct SubSysStatusElement
   {
     Gtk::Label* name;
     Gtk::Label* status;
 
   };
      //* System Overview
-  struct NodeStatusGrid
+  struct SubSysStatusGrid
   {
     Gtk::Grid* grid;
-    NodeStatusElement nodes[NUM_MONITORED_NODES];
+    SubSysStatusElement system[NUM_MONITORED_SYSTEMS];
   };
 
 
-  NodeStatusGrid node_stats;
+  SubSysStatusGrid system_health;
 
 
   //? Ros2 stuffs
