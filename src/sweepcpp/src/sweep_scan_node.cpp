4b6d8d1b72d929762b2122fa977f0154c7a0bcd8
Roozki <zawadzkirowan@gmail.com>
2025-06-08 16:15:59 -0700

    what I thought these were in the previous commit

--- src/sweepcpp/src/sweep_scan_node.cpp
+++ src/sweepcpp/src/sweep_scan_node.cpp
@@ -20,7 +20,7 @@ public:
     // Declare parameters
     this->declare_parameter("topic", "scan");
     this->declare_parameter("serial_port", "/dev/serial/by-id/usb-FTDI_FT230X_Basic_UART_DM00L4EB-if00-port0"); // usbport /dev/ttyUSB0 
-    this->declare_parameter("rotation_speed", 5); // 1-10
+    this->declare_parameter("rotation_speed", 10); // 1-10
     this->declare_parameter("sample_rate", 1000); //500-1750
     this->declare_parameter("frame_id", "lidar");
 
