8c6fed570d4abdf06fc8112279ba26332b2ebb98
Roozki <zawadzkirowan@gmail.com>
2025-07-26 16:39:12 -0700

    Fixed zoom module to use services, added a launch file

---
+++ src/ipcamerafeed/launch/ipcam_launch.launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='ipcamerafeed',
+            executable='ipcamera',  
+            name='ipcamera'
+        ),
+        Node(
+            package='ipcamerafeed',
+            executable='ipcamerazoom',  
+            name='ipcamerazoom'
+        ),
+        Node(
+            package='image_publisher',
+            executable='image_publisher_node',  
+            name='image_publisher',
+            parameters=[{'filename' : 'rtsp://192.168.0.95/stream0'}]
+        ),
+        Node(
+            package='image_view',
+            executable='image_view',
+            name='ipcamera_view',
+            remappings=[
+                ('image', '/image_raw') # image raw ropic from the image publisher
+            ]
+        )
+    ])
