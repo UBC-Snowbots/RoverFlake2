329af8674a74514bc9317f9f45ec5f0bdcfcfec1
Roozki <zawadzkirowan@gmail.com>
2025-08-31 12:28:39 -0700

    sample packge updates

--- src/rover_samples/rv_sample_package/src/sample_node.cpp
+++ src/rover_samples/rv_sample_package/src/sample_node.cpp
@@ -2,16 +2,19 @@
 #include "sample_node.h"
 
 // Constructor
-SampleNode::SampleNode() : Node("sample_node") { // We create a class using rclcpp::Node as a base class. You can still use another base class if you need, albeit sometimes with difficulties in passing args..
+SampleNode::SampleNode() : Node("sample_node") 
+{ // We create a class using rclcpp::Node as a base class. You can still use another base class if you need, albeit sometimes with difficulties in passing args..
+   
     // Quality of service example
     auto qos = rclcpp::QoS(rclcpp::KeepLast(1)).transient_local();
 
-    double period = 1.0/CONTROL_RATE_HZ;
-    // timer_ = this->create_wall_timer(
-    // std::chrono::duration<double>(period),std::bind(&ManualControlNode::test_send, this));
-    g29_subscriber_ = this->create_subscription<sensor_msgs::msg::Joy>(
+    joy_subscriber = this->create_subscription<sensor_msgs::msg::Joy>(
         "/joy", qos /* or an int for quick qos*/, std::bind(&SampleNode::joy_callback, this, std::placeholders::_1));
-
+    
+    cmd_vel_pub = this->create_publisher<geometry_msgs::msg::TwistStamped>("/cmd_vel", qos);
+        
+        timer_ = this->create_wall_timer(
+        std::chrono::duration<double>(TIMER_PERIOD_MS),std::bind(&SampleNode::timer_callback, this));
 
     // parameters:
     this->declare_parameter("param_name", std::vector<std::string>({"Watchdog"}));
@@ -20,7 +23,8 @@ SampleNode::SampleNode() : Node("sample_node") { // We create a class using rclc
 
 
 // Main function (entry point of node)
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
     // Init ros2 with args. 
     rclcpp::init(argc, argv);
 
@@ -34,3 +38,44 @@ int main(int argc, char *argv[]) {
     rclcpp::shutdown();
     return 0;
 }
+
+// Member function implementations (sub and timer callbacks go first)
+
+void SampleNode::joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
+{
+    float steering_angle = msg->axes[0];
+    float speed = 99.0 - (msg->axes[3] + 1)*100;
+    float acceleration = (msg->axes[2] + 1)*20 - (msg->axes[3] + 1)*20;
+    float jerk = (msg->axes[2] + 1)*20 - (msg->axes[3] + 1)*20;
+    int paddleR = msg->buttons[4];
+    int paddleL = msg->buttons[5];
+    send_generic_command(steering_angle, speed, acceleration, jerk);
+    
+}
+
+void SampleNode::timer_callback()
+{
+    std::cout << "Node is running (std cout)" << std::endl; // Standard C++ is always available
+    RCLCPP_INFO(this->get_logger(), "Node is running (ros2 info)"); // ROS2 (rclcpp) also has functions/macros that can replace some of the standard c++ workflow.
+    RCLCPP_WARN(this->get_logger(), "Node is running (ros2 warn)"); // ROS2 (rclcpp) also has functions that can replace some of the standard c++ workflow.
+    RCLCPP_ERROR(this->get_logger(), "Node is running (ros2 error)"); // ROS2 (rclcpp) also has functions that can replace some of the standard c++ workflow.
+}
+
+
+void SampleNode::send_generic_command(float steering_angle, float speed, float acceleration, float jerk) 
+{
+    // msg.steering angle = steering angle;
+    // etc...
+    // some_publisher.pub(msg); 
+}
+
+
+
+
+
+
+
+// Other examples/tips
+
+//params can be refreshed at runtime
+// std::vector<std::string> expected_nodes = this->get_parameter("expected_nodes").as_string_array();
