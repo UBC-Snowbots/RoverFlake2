8556b7dadfbbbad6f045c2dddd1b7b7487748c04
Roozki <zawadzkirowan@gmail.com>
2025-09-09 23:40:10 -0700

    rstein

--- setup_scripts/install-ros2-humble.sh
+++ setup_scripts/install-ros2-humble.sh
@@ -23,14 +23,13 @@ else
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
     sudo apt update
-
     sudo apt upgrade
 
     sudo apt install ros-humble-desktop
-
     sudo apt install ros-dev-tools
 
     echo source /opt/ros/humble/setup.bash >> ~/.bashrc
+    source ~/.bashrc
 fi
 
 echo install-ros2-humble.sh complete.
