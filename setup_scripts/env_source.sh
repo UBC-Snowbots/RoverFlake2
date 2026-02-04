0699d56ac338048f12d5bd9365651daec3f11ef4
Roozki <zawadzkirowan@gmail.com>
2026-02-03 21:25:10 -0800

    background

--- setup_scripts/env_source.sh
+++ setup_scripts/env_source.sh
@@ -2,3 +2,8 @@
 source /opt/ros/humble/setup.bash
 source /home/${USER}/RoverFlake2/install/setup.bash
 source /home/${USER}/RoverFlake2/network_stuff/cyclonedds_env.sh
+
+
+# gui apps
+export DISPLAY=:0
+export XAUTHORITY="/run/user/1001/gdm"
