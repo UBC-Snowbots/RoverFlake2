945e16561ec7e8e2d89211e6edc5320c437c412b
Aaron Rhim <rhimaaron@gmail.com>
2026-02-14 21:46:39 -0800

    updated docker compose with a service that supports GUI rendering

--- setup_scripts/submodule_update.sh
+++ setup_scripts/submodule_update.sh
@@ -1,1 +1,7 @@
+# required for docker build
+if [ ! -d "${ROVERFLAKE_ROOT}/.git" ]; then
+    echo "Not a git repository, skipping submodule update"
+    exit 0
+fi
+
 git submodule update --init --recursive
