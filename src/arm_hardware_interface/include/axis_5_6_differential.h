0aa5071b78981d950b59ad7117bdfc40b869f0f0
laurenkeilty <laurenkeilty8@gmail.com>
2026-02-15 14:21:28 -0800

    Adding Differential Drive Helper Function

---
+++ src/arm_hardware_interface/include/axis_5_6_differential.h
@@ -0,0 +1,32 @@
+def differential_drive(axis5_input, axis6_input):
+    """
+    Converts differential drive requests to motor outputs.
+    Directions given from bird eye view and top down view of each motor.
+
+    ex. A5 = -5, A6 = -3 means spin A5 -5 R to L and A6 down by 3.
+    This requires M5 to move 3 from L to R (positive direction) and M6 to move 3 from R to L (negative direction)
+    from a top down view of each motor. The A5 spin also requires M5 to move 5 more in the positive direction. 
+    (We choose to move the motor to execute the spin as the one in the current direction of motions)
+
+    takes:
+        axis5_input: Unitless input for axis 5 
+        axis6_input: Unitless input for axis 6
+    returns: 
+        (motor5_output, motor6_output) Unitless values for motors 5 and 6
+        
+    """
+    // If the inputs have the same sign, to continue in the same direction of motion, use axis 5 for rotation
+    if((axis5_input > 0 && axis6_input > 0) || (axis5_input > 0 && axis6_input > 0)) {
+
+        motor5 = - axis5_input - axis6_input
+        motor6 = axis5_input
+
+    }
+    // Else, use axis 6 for rotation
+    else {
+
+        motor5 = - axis5_input
+        motor6 = axis5_input - axis6_input
+
+    }  
+    return motor5, motor6
