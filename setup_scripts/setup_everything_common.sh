6fdb846e21e3f30440bc151d1db3d387d2464f02
Roozki <zawadzkirowan@gmail.com>
2026-03-27 12:58:56 -0700

    still trying to find error

--- setup_scripts/setup_everything_common.sh
+++ setup_scripts/setup_everything_common.sh
@@ -33,7 +33,7 @@ echo This script will NOT install discord. run install-discord.sh if you want di
 sleep 1.0
 echo starting with ros2...
 
-sudo apt update
+sudo apt update -y
 cd $ROVERFLAKE_ROOT/setup_scripts/
 bash install_dependencies.sh
 bash install-ros2-humble.sh # also runs apt update, if ros2 is not installed
