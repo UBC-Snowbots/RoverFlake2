abdf69bfcba3d017d2bfe1f73cea6ed3028badb5
epw1624 <emily.wood1624@gmail.com>
2026-05-19 20:31:51 -0700

    Hysteresis implementation

--- src/drive_control/src/wheel_speed.cpp
+++ src/drive_control/src/wheel_speed.cpp
@@ -10,7 +10,11 @@ WheelSpeedNode::WheelSpeedNode() : Node("wheel_speed_node") {  // Initialize the
     
     // Create a subscription to receive Twist messages (which contain velocity commands) from the "/cmd_vel" topic
     cmd_vel_sub_ = this->create_subscription<geometry_msgs::msg::Twist>(
-        "/cmd_vel", 10, std::bind(&WheelSpeedNode::cmdVelCallback, this, std::placeholders::_1));  // Call cmdVelCallback when a new message is received
+        "/cmd_vel", 10, std::bind(&WheelSpeedNode::cmdVelCallback, this, std::placeholders::_1));
+
+    // Create a subscription to the drive/feedback topic to keep track of wheel speed history for Schmitt trigger
+    drive_feedback_sub_ = this->create_subscription<rover_msgs::msg::DriveFeedback>(
+        "drive/feedback", 10, std::bind(&WheelSpeedNode::driveFeedbackCallback, this, std::placeholders::_1));
 }
 
 // Callback function that processes velocity commands received in the form of Twist messages
@@ -18,37 +22,79 @@ void WheelSpeedNode::cmdVelCallback(const geometry_msgs::msg::Twist::SharedPtr m
     // Extract the linear and angular velocity from the incoming message
     double linear = msg->linear.x;
     double angular = msg->angular.z;
-    
-    // // Calculate a factor to adjust wheel speed based on the wheel radius and angular velocities
-    // double wheel_speed_factor = (360.0 / WHEEL_RADIUS_METERS) * (M_PI / 2.0) * 0.0001;  // Placeholder for speed scaling
 
-    double wheel_speed_factor =  0.1;  // Placeholder for speed scaling
+    WheelVelocities wheel_velocities = tankDrive(linear, angular);
+    wheel_velocities = applyHysteresis(current_wheel_velocities, wheel_velocities);
 
-    // Apply the speed factor to both linear and angular velocities
-    linear *= wheel_speed_factor;
-    angular *= wheel_speed_factor;
-
-    // Create vectors of motor commands that correspond to the calculated speeds for left and right wheels
-    std::vector<double> left_wheel_commands = {
-        -linear + angular,   // Left Front Wheel
-        -linear + angular,   // Left Mid Wheel
-        -linear + angular    // Left Rear Wheel
-    };
-    
-    std::vector<double> right_wheel_commands = {
-        linear + angular,  // Right Front Wheel
-        linear + angular,  // Right Mid Wheel
-        linear + angular   // Right Rear Wheel
-    };
-
-    // Publish the separate left and right wheel speed messages
+    // Publish the separate left and right wheel velocity messages
     std_msgs::msg::Float64MultiArray left_msg;
-    left_msg.data = left_wheel_commands;  // Set left wheel commands data
-    left_wheel_pub_->publish(left_msg);   // Publish to "/left_wheel_speeds"
+    left_msg.data = wheel_velocities.left_wheel_velocities;
+    left_wheel_pub_->publish(left_msg);
 
     std_msgs::msg::Float64MultiArray right_msg;
-    right_msg.data = right_wheel_commands;  // Set right wheel commands data
-    right_wheel_pub_->publish(right_msg);   // Publish to "/right_wheel_speeds"
+    right_msg.data = wheel_velocities.right_wheel_velocities;
+    right_wheel_pub_->publish(right_msg);
+}
+
+void WheelSpeedNode::driveFeedbackCallback(const rover_msgs::msg::DriveFeedback::SharedPtr msg) {
+    for (int i = 0; i < NUM_MOTORS / 2; i++) {
+        current_wheel_velocities.left_wheel_velocities[i] = msg->velocities[i];
+        current_wheel_velocities.right_wheel_velocities[i] = msg->velocities[NUM_MOTORS / 2 + i];
+    }
+}
+
+WheelVelocities WheelSpeedNode::tankDrive(double linear, double angular) {
+    WheelVelocities wheel_velocities;
+
+    double l = -linear + angular;
+    double r = linear + angular;
+
+    wheel_velocities.left_wheel_velocities = { l, l, l };
+
+    wheel_velocities.right_wheel_velocities = { r, r, r };
+
+    return wheel_velocities;
+}
+
+WheelVelocities WheelSpeedNode::applyHysteresis(WheelVelocities current, WheelVelocities target) {
+    WheelVelocities final_wheel_velocities;
+    final_wheel_velocities.left_wheel_velocities.resize(NUM_MOTORS / 2);
+    final_wheel_velocities.right_wheel_velocities.resize(NUM_MOTORS / 2);
+
+    auto applyHysteresisHelper = [&](double current, double target) {
+        double abs_current = std::abs(current);
+        double abs_target = std::abs(target);
+
+        if (abs_target < MOTOR_STOP_THRESHOLD) return 0.0;
+        else if (abs_target < MOTOR_START_THRESHOLD) {
+            if (abs_current > MOTOR_STOP_THRESHOLD) {
+                // Keep moving in same direction at MOTOR_START_THRESHOLD
+                return (target > 0) ? MOTOR_START_THRESHOLD : -MOTOR_START_THRESHOLD;
+            }
+            else return 0.0; // Keep not moving
+        }
+        else return target;
+    };
+
+    for (int i = 0; i < NUM_MOTORS / 2; i++) {
+        final_wheel_velocities.left_wheel_velocities[i] = applyHysteresisHelper(current.left_wheel_velocities[i], target.left_wheel_velocities[i]);
+        final_wheel_velocities.right_wheel_velocities[i] = applyHysteresisHelper(current.right_wheel_velocities[i], target.right_wheel_velocities[i]);
+    }
+
+    return final_wheel_velocities;
+}
+
+WheelVelocities WheelSpeedNode::ackermann(double linear, double angular) {
+    WheelVelocities wheel_velocities;
+
+    double l = -linear + angular;
+    double r = linear + angular;
+
+    wheel_velocities.left_wheel_velocities = { l, l, l };
+
+    wheel_velocities.right_wheel_velocities = { r, r, r };
+
+    return wheel_velocities;
 }
 
 // Main function to initialize the ROS2 node and start processing
