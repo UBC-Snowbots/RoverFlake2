928774f0dc924ed7e99dfb91401a73431acfbe11
epw1624 <emily.wood1624@gmail.com>
2026-07-14 22:15:43 -0700

    Create publisher and timer for magnetometer data

--- src/comms_base_control/include/comms_base_control/death_ray_magnetometer.h
+++ src/comms_base_control/include/comms_base_control/death_ray_magnetometer.h
@@ -1,7 +1,11 @@
 #ifndef DEATH_RAY_MAGNETOMETER_H
 #define DEATH_RAY_MAGNETOMETER_H
 
+#include "rclcpp/qos.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float32.hpp"
+
+#define HEADING_FEEDBACK_PUBLISH_FREQUENCY_MS 200
 
 /**
  * @brief DeathRayMagnetometerNode reads the direction the death ray is facing
@@ -13,6 +17,13 @@ public:
     ~DeathRayMagnetometerNode();
 
 private:
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr heading_pub_;
+
+    // Function to publish the current heading of the death ray
+    void publishHeadingFeedback();
+
+    rclcpp::TimerBase::SharedPtr heading_feedback_timer_;
+
     float heading = 0.0;
 };
 
--- src/comms_base_control/src/death_ray_magnetometer.cpp
+++ src/comms_base_control/src/death_ray_magnetometer.cpp
@@ -1,12 +1,26 @@
 #include "death_ray_magnetometer.h"
 
 DeathRayMagnetometerNode::DeathRayMagnetometerNode() : Node("death_ray_magnetometer_node") {
-    // Placeholder for real constructor while I bootstrap the node
-    RCLCPP_INFO(this->get_logger(), "Starting magnetometer node");
+    heading_pub_ = this->create_publisher<std_msgs::msg::Float32>("death_ray_heading", 10);
+
+    heading_feedback_timer_ = this->create_wall_timer(
+        std::chrono::milliseconds(HEADING_FEEDBACK_PUBLISH_FREQUENCY_MS),
+        std::bind(&DeathRayMagnetometerNode::publishHeadingFeedback, this)
+    );
 }
 
 DeathRayMagnetometerNode::~DeathRayMagnetometerNode() {
+    // Placeholder for node cleanup
+    // Explicit destructor will be needed to clean up GPIO/serial connections
+}
 
+void DeathRayMagnetometerNode::publishHeadingFeedback() {
+    // TODO: update this to actually read heading from the GPIO pins
+    // This is for sanity checking the node on my laptop
+    std_msgs::msg::Float32 message;
+    message.data = 180.0;
+
+    heading_pub_->publish(message);
 }
 
 int main(int argc, char* argv[]) {
