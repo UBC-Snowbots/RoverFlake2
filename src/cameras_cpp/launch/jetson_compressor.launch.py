67ff3b0717856e8c488b35a7c806e60f93f36b39
BenUBC <ben5@student.ubc.ca>
2026-07-29 16:34:19 -0700

    add multiple camera support

--- src/cameras_cpp/launch/jetson_compressor.launch.py
+++ src/cameras_cpp/launch/jetson_compressor.launch.py
@@ -3,27 +3,30 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy
+from ..config.cameras import CAMERAS
 
 def generate_launch_description():
+    nodes = []
 
-    test_params = os.path.join(
-    get_package_share_directory('cameras_cpp'),
-    'config', 'jetson_test_params.yaml'
-    )
+    for camera_name, camera_params_file in CAMERAS:
+        camera_params_path = os.path.join(
+            get_package_share_directory('cameras_cpp'),
+            'config', camera_params_file
+        )
+        camera_node = create_camera_node(camera_name, camera_params_path)
+        nodes.append(camera_node)
 
-    feed_test = Node(
+    return LaunchDescription(nodes)
+
+def create_camera_node(camera_name, camera_params):
+    return Node(
         package='v4l2_camera',
         executable='v4l2_camera_node',
-        name='test_camera_node',
+        name=camera_name,
         output='screen',
-        parameters=[test_params],
+        parameters=[camera_params],
         remappings=[
-            ('/image_raw', '/vehicle_1/rear_feed/image_raw'),  
-            ('/image_raw/ffmpeg', '/vehicle_1/rear_feed/image_raw/ffmpeg'),
-            # Add more remappings here if needed
+            ('/image_raw', f'/vehicle_1/{camera_name}/image_raw'),
+            ('/image_raw/ffmpeg', f'/vehicle_1/{camera_name}/image_raw/ffmpeg'),
         ],
     )
-
-    return LaunchDescription([
-        feed_test
-    ])
--- src/cameras_cpp/launch/jetson_decompressor.launch.py
+++ src/cameras_cpp/launch/jetson_decompressor.launch.py
@@ -1,22 +1,25 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-def generate_launch_description():
-    broadcast_decoder_0 = Node(
-        package='image_transport',
-        executable='republish',
-        name='test_decoder_node',
-        remappings=[
-            ('in/ffmpeg', '/vehicle_1/rear_feed/image_raw/ffmpeg'),
-            ('out', '/vehicle_1/rear_feed/image_raw_decoded'),
-        ],
-        parameters=[
-            {'in.ffmpeg.decoders.h264': 'h264'},
-        ],
-        output='screen',
-        arguments=['ffmpeg', 'raw']
-    )
+from ..config.cameras import CAMERAS
 
-    return LaunchDescription([
-        broadcast_decoder_0,
-    ])
+def generate_launch_description():
+    broadcastor_nodes = []
+    for camera_name, _ in CAMERAS:
+        broadcastor_node = Node(
+            package='image_transport',
+            executable='republish',
+            name=f'{camera_name}_decoder_node',
+            remappings=[
+                ('in/ffmpeg', f'/vehicle_1/{camera_name}/image_raw/ffmpeg'),
+                ('out', f'/vehicle_1/{camera_name}/image_raw_decoded'),
+            ],
+            parameters=[
+                {'in.ffmpeg.decoders.h264': 'h264'},
+            ],
+            output='screen',
+            arguments=['ffmpeg', 'raw']
+        )
+        broadcastor_nodes.append(broadcastor_node)
+
+    return LaunchDescription(broadcastor_nodes)
