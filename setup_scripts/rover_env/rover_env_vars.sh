4ef91df07fa6db4cba40d214b2078b91d4d28ca2
Aaron Rhim <rhimaaron@gmail.com>
2026-08-02 18:45:26 -0700

    feat(setup_scripts): default ROS_DOMAIN_ID=101 in rover env

--- setup_scripts/rover_env/rover_env_vars.sh
+++ setup_scripts/rover_env/rover_env_vars.sh
@@ -1,4 +1,7 @@
 # All our common enviroment variables
 export COLCON_DEFAULTS_FILE="${ROVERFLAKE_ROOT}/colcon.defaults.yaml"
 
+# Rover operational DDS domain. Pre-exported values win (e.g. bench isolation).
+export ROS_DOMAIN_ID="${ROS_DOMAIN_ID:-101}"
+
 export RANDOM_INSTALL_FILE_DIR="${ROVERFLAKE_ROOT}/random_install_files"
