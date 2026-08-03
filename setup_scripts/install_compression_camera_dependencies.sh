019eb2fb04352848847f9a52737bbf5d60e5eba5
Aaron Rhim <rhimaaron@gmail.com>
2026-08-02 20:36:32 -0700

    chore(setup): ffmpeg image transport + msgs for compression cameras

--- setup_scripts/install_compression_camera_dependencies.sh
+++ setup_scripts/install_compression_camera_dependencies.sh
@@ -1,2 +1,3 @@
-sudo apt install ros-humble-image-transport-plugins -y
+sudo apt install ros-humble-ffmpeg-image-transport-plugins -y
+sudo apt install ros-humble-image-transport-msgs -y
 sudo apt install ros-humble-camera-ros -y
