7f5cbaa50f06e7cfdbcf8a45c142ea8b1af57268
Cameron Basara <cameronbasara@gmail.com>
2026-08-03 21:21:19 -0700

    pre dep dep

--- setup_scripts/install_dependencies.sh
+++ setup_scripts/install_dependencies.sh
@@ -13,5 +13,6 @@ sudo apt-get install -y libphidget22-dev
 sudo apt install -y libcamera-dev
 
 # for gscam
+sudo apt install -y libunwind-dev
 sudo apt-get install -y gstreamer1.0-tools libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-good1.0-dev 
 sudo apt install -y ros-humble-gscam 
