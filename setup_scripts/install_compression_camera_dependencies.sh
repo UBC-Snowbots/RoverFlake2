c0c0d61834507a67fea918598bda836901296a7a
Aaron Rhim <rhimaaron@gmail.com>
2026-08-07 00:45:07 -0700

    revert install_compression_camera_dependencies.sh to main

--- setup_scripts/install_compression_camera_dependencies.sh
+++ setup_scripts/install_compression_camera_dependencies.sh
@@ -1,5 +1,3 @@
-sudo apt install ros-humble-ffmpeg-image-transport-plugins -y
 sudo apt install ros-humble-image-transport-plugins -y
-sudo apt install ros-humble-image-transport-msgs -y
-sudo apt install ros-humble-ffmpeg-image-transport-msgs -y
 sudo apt install ros-humble-camera-ros -y
+sudo apt install ros-humble-ffmpeg-image-transport-msgs -y
