77b5f78fa4dde8eaf4aa22d331d0a44bf72c25f7
Aaron Rhim <rhimaaron@gmail.com>
2026-08-06 23:26:34 -0700

    hi

--- setup_scripts/rover_env/rover_env_vars.sh
+++ setup_scripts/rover_env/rover_env_vars.sh
@@ -2,6 +2,6 @@
 export COLCON_DEFAULTS_FILE="${ROVERFLAKE_ROOT}/colcon.defaults.yaml"
 
 # Rover operational DDS domain. Pre-exported values win (e.g. bench isolation).
-export ROS_DOMAIN_ID="${ROS_DOMAIN_ID:-101}"
+export ROS_DOMAIN_ID="${ROS_DOMAIN_ID:101}"
 
 export RANDOM_INSTALL_FILE_DIR="${ROVERFLAKE_ROOT}/random_install_files"
